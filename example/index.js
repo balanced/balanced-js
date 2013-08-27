c39b05b35e4ec477f3edddc9cbe6a6a688d94df8
Marshall Jones <marshall@balancedpayments.com>
2013-08-27 12:14:14 -0700

    tweak example

--- example/index.js
+++ example/index.js
@@ -1,46 +1,42 @@
 $(document).ready(function() {
-    balanced.init('/v1/marketplaces/TEST-MP5noKWGqLyLOLKkQkJmKg9s', {
+    balanced.init({
         server: 'http://localhost:3000'
     });
 
-    $("#cc-submit").click(function(e) {
+    $('#cc-submit').click(function(e) {
         e.preventDefault();
 
-        $("#response").slideUp(200);
+        $('#response').slideUp(200);
 
         var payload = {
-            card_number: $("#cc-number").val(),
-            expiration_month: $("#cc-ex-month").val(),
-            expiration_year: $("#cc-ex-year").val(),
-            security_code: $("#ex-csc").val()
+            card_number: $('#cc-number').val(),
+            expiration_month: $('#cc-ex-month').val(),
+            expiration_year: $('#cc-ex-year').val(),
+            security_code: $('#ex-csc').val()
         };
 
-        ////
         // Tokenize card
-        ////
-        balanced.card.create(payload, function(data, errors, status) {
-            $("#response .panel-body pre").html(JSON.stringify(data, false, 4));
-            $("#response").slideDown(300);
+        balanced.card.create(payload, function(response) {
+            $('#response .panel-body pre').html(JSON.stringify(response, false, 4));
+            $('#response').slideDown(300);
         });
     });
 
-    $("#ba-submit").click(function(e) {
+    $('#ba-submit').click(function(e) {
         e.preventDefault();
 
-        $("#response").slideUp(200);
+        $('#response').slideUp(200);
 
         var payload = {
-            name: $("#ba-name").val(),
-            account_number: $("#ba-number").val(),
-            routing_number: $("#ba-routing").val()
+            name: $('#ba-name').val(),
+            account_number: $('#ba-number').val(),
+            routing_number: $('#ba-routing').val()
         };
 
-        ////
         // Tokenize bank account
-        ////
-        balanced.bankAccount.create(payload, function(data, errors, status) {
-            $("#response .panel-body pre").html(JSON.stringify(data, false, 4));
-            $("#response").slideDown(300);
+        balanced.bankAccount.create(payload, function(response) {
+            $('#response .panel-body pre').html(JSON.stringify(response, false, 4));
+            $('#response').slideDown(300);
         });
     });
 });
