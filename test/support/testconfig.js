c5ccb79aa9efd266ece3a3052a193d714471e88e
Justin Keller <hello@nodesocket.com>
2013-09-04 14:18:39 -0700

    Tweaked testconfig.js logging text.

--- test/support/testconfig.js
+++ test/support/testconfig.js
@@ -1,9 +1,9 @@
 QUnit.testStart(function (test) {
     var module = test.module ? test.module : '';
-    console.log('#' + module + ' ' + test.name + ': starting setup.');
+    console.log('#' + module + ' ' + test.name + ': starting.');
 });
 
 QUnit.testDone(function (test) {
     var module = test.module ? test.module : '';
-    console.log('#' + module + ' ' + test.name + ': tearing down.');
+    console.log('#' + module + ' ' + test.name + ': finished.');
 });
