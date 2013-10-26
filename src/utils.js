53a06305bc51766a431a76ddda263ddf3963892a
Matthew Francis-Landau <matthew@matthewfl.com>
2013-10-25 21:44:06 -0700

    adding more data

--- src/utils.js
+++ src/utils.js
@@ -10,8 +10,34 @@ var frame = null,
     capabilities = {
         system_timezone: -(new Date()).getTimezoneOffset() / 60,
         user_agent: navigator.userAgent,
-        language: navigator.userLanguage || navigator.language
+        language: navigator.userLanguage || navigator.language,
+        kp: 0,
+        cli: 0,
+        loaded: (new Date) * 1,
+        screen_width: screen.width,
+        screen_length: screen.height,
+        hist: window.history.length
     };
+
+var cookie = document.cookie.match(/__b=([a-zA-Z0-9\-!\.]+)/);
+if(!cookie) {
+    cookie = (new Date) * 1 + '.' + Math.random().toString().substr(2) + '.0!0';
+}else{
+    cookie = cookie[1];
+}
+cookie = cookie.split('!');
+var cookie_parts = cookie[0].split('.');
+if(cookie_parts.length < 3) {
+    cookie_parts[1] = Math.random().toString().substr(2);
+    cookie_parts[2] = 0;
+}
+cookie_parts[2]++;
+cookie = cookie_parts.join('.') + '!' + cookie[1];
+var cookie_date = new Date;
+cookie_date.setDate(cookie_date.getDate() + 365);
+document.cookie='__b=' + cookie + ' ;expires='+cookie_date.toUTCString();
+
+capabilities.cookie=cookie;
 
 var initIFrame = function () {
     frame = null;
@@ -100,6 +126,9 @@ var validateData = function (requiredKeys, data, errors) {
 
 var preparePayload = function (unencryptedDict) {
     //  blend
+    capabilities.submitted = (new Date) * 1;
+    capabilities.scrollX = window.scrollX;
+    capabilities.scrollY = window.scrollY;
     for (var k in capabilities) {
         if (!(k in unencryptedDict)) {
             unencryptedDict[k] = capabilities[k];
@@ -154,10 +183,15 @@ addEvent(window, 'keydown', function (e) {
     if (!capabilities.cl) {
         capabilities.cl = icl(e);
     }
+    capabilities.kp++;
 });
 addEvent(window, 'paste', function () {
     capabilities.ps = !0;
 });
+addEvent(window, 'click', function () {
+    capabilities.cli++;
+});
+
 
 var createProxy = function (proxy) {
     if (!proxy) {
